730b6ccf2e1e74530cf089b8e87defec4cd7a789
carlosaw <carlosfreevg@gmail.com>
2023-01-04 10:35:32 -0400

    vercel.json

--- src/index.js
+++ src/index.js
@@ -3,25 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './context';
 
-// Add Express
-import express from "express";
-
-// Initialize Express
-const app = express();
-
-// Create GET request
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
-
-// Initialize server
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
-
-// Export the Express API
-module.exports = app;
-
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
