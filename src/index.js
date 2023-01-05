87109094086aa3d53ef7571060eb1b01e2a93b78
carlosaw <carlosfreevg@gmail.com>
2023-01-05 10:20:20 -0400

    running

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './context';
+import express from "express";
+import { Router } from "express";
+
+const app = express();
+
+const route = Router();
+
+route.get('/', (req, res) => {
+  return res.json({
+    success: true,
+    message: "Sucesso!"
+  })
+});
+app.use(route);
+
+app.listen(4000, () => {
+  console.log("Server running in " + 4000)
+})
 
 ReactDOM.render(
   <React.StrictMode>
